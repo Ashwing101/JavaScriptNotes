bb6f576f75c2781a16fd67545b892172fb43edcf
Ashwini <gourashwini115@gmail.com>
2025-05-04 17:48:06 -0400

    DOM manipulation

---
+++ 06-dom/script.js
@@ -0,0 +1,10 @@
+console.log(window);
+console.log(window.document);
+console.dir(document.body.innerHTML);
+
+console.log(document.links[0].href);
+
+document.body.innerHTML= `<h1>Hello World</>`;
+
+document.write('Hello form JS');
+
