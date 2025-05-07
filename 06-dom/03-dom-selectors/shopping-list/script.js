704a6d3abbf49c7ad92824c323c572c16c3755e7
Ashwini <gourashwini115@gmail.com>
2025-05-07 00:10:34 -0400

    DOM Style

---
+++ 06-dom/03-dom-selectors/shopping-list/script.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById('item-list'));
+
+//Set Attributes
+document.getElementById('app-title').title ='Shopping List';
+document.getElementById('app-title').setAttribute('class', 'title');
+
+
+const title = document.getElementById('page-title');
+console.log(title.textContent);
+//Get/change content
+
