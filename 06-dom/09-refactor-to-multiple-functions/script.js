f75d30a323b6ffd7f1343c63f2e72c7ba0a1fba0
Ashwini <gourashwini115@gmail.com>
2025-05-08 03:15:05 -0400

    Add Element after before starting and at end

---
+++ 06-dom/09-refactor-to-multiple-functions/script.js
@@ -0,0 +1,5 @@
+//Create a each function for each functionality for the previous code
+// example
+
+//Create function for button
+// Create function for Icon 
---
+++ 06-dom/10-insert-elements-dom/script.js
@@ -0,0 +1,40 @@
+//insertAdjacentElement Example
+
+function insertElement(){
+    const filter = document.querySelector('.filter');
+
+    const h1 = document.createElement('h1');
+
+    h1.textContent ='insertAdjacentElement';
+
+    filter.insertAdjacentElement('beforebegin'. h1);
+}
+
+//insertAdjacentText Example
+function insertText(){
+    const item = document.querySelector('li:first-child');
+
+    item.insertAdjacentText('afterend' , 'insertAdjacentText');
+}
+
+//insertAdjacentHTML Example
+
+function insertHtml(){
+    const clearBtn = document.querySelector('#clear');
+
+    clearBtn.insertAdjacentHTML('afterend' , '<h2>insertAdjacentHTML</h2>');
+}
+
+//insertBefore Example
+function insertBeforeItem(){
+    const ul = document.querySelector('ul');
+
+    const li = document.createElement('li');
+    li.textContent = 'insertBefore';
+
+    const tirdItem =  document.querySelector('li-nth-child(3)');
+
+    ul.insertBefore(li, thirdItem);
+}
+
+
